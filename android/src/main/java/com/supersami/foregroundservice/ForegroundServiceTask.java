3d8e52c48a995d447d3e9f1d141d2cbc2733ce75
EmanuelClaesson <71439206+EmanuelClaesson@users.noreply.github.com>
2021-11-04 13:45:15 +0100

    Update ForegroundServiceTask.java

--- android/src/main/java/com/supersami/foregroundservice/ForegroundServiceTask.java
+++ android/src/main/java/com/supersami/foregroundservice/ForegroundServiceTask.java
@@ -26,7 +26,7 @@ public class ForegroundServiceTask extends HeadlessJsTaskService {
             return new HeadlessJsTaskConfig(
                 extras.getString("taskName"),
                     Arguments.fromBundle(extras),
-                    5000, // timeout for the task
+                    0, // timeout for the task
                     true // optional: defines whether or not  the task is allowed in foreground. Default is false
             );
         }
